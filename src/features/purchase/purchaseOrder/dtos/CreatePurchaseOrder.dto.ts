f6160cd37ceb6dcb0bd3d58c6a0781e6b6af7f14
Chest Piece <anasaltaf674@gmail.com>
2026-01-16 19:25:44 +0500

    fixed purchase order

--- src/features/purchase/purchaseOrder/dtos/CreatePurchaseOrder.dto.ts
+++ src/features/purchase/purchaseOrder/dtos/CreatePurchaseOrder.dto.ts
@@ -1,30 +1,51 @@
-/* eslint-disable prettier/prettier */
-import { ApiProperty } from "@nestjs/swagger"
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsNumber,
+  IsDateString,
+  IsArray,
+  IsObject,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-import { Product } from "src/features/products/entities/Product.entity"
+import { Product } from 'src/features/products/entities/Product.entity';
 
-import { Supplier } from "../../supplier/supplier.entity";
+import { Supplier } from '../../supplier/supplier.entity';
 
 export class CreatePurchaseOrderDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  poNumber: string;
 
-    poNumber: string;
+  @ApiProperty()
+  @IsOptional()
+  poDate: Date;
 
-    @ApiProperty()
-    poDate: Date;
+  @ApiProperty()
+  @IsOptional()
+  expectedDelivery: Date;
 
-    @ApiProperty()
-    expectedDelivery: Date
+  @ApiProperty({ type: Object })
+  @IsOptional()
+  supplier: Supplier;
 
-    @ApiProperty({ type: Object })
-    supplier: Supplier
+  @ApiProperty()
+  @IsArray()
+  @IsOptional()
+  products: Product[];
 
-    @ApiProperty()
-    products: Product[]
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  remarks: string;
 
-    @ApiProperty()
-    remarks: string
+  @IsNumber()
+  @IsOptional()
+  subTotal: number;
 
-    subTotal: number
-
-    total: number
+  @IsNumber()
+  @IsOptional()
+  total: number;
 }
